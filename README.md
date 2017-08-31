f009696958e3324d78cb767d2ae04472723f7c5f
Username <username@NI-NTB-118.local>
2017-08-31 12:23:56 -0300

    First commit - Functional selenium test in Chrome

---
+++ README.md
@@ -0,0 +1,12 @@
+# Java-Demo-Selenium
+
+This is a simple WebProject with Gradle and Selenium that simulates a user calculating the perimeter and area of a rectangle.
+
+## How to run the test 
+
+`./gradlew test`
+
+In the code you will need to configure the webdriver. You can download it from: https://sites.google.com/a/chromium.org/chromedriver/downloads
+
+### NOTE
+This is a empty application. If you try to build it, you will receive an error
---
+++ src/test/java/com/demo/app/SeleniumTest.java
@@ -0,0 +1,61 @@
+package com.demo.app;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver.Options;
+import org.openqa.selenium.WebDriver.Timeouts;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+public class SeleniumTest
+{
+  //@Autowired
+  private static WebDriver wd;
+  
+  @Before
+  public void setUp()
+    throws Exception
+  {
+    System.setProperty("webdriver.chrome.driver", "/Users/username/Desktop/selenium/chromedriver");
+    wd = new ChromeDriver();
+    wd.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+  }
+  
+  @Test
+  public void SeleniumTest()
+  {
+    wd.get("http://localhost:8080/");
+    wd.findElement(By.linkText("Enter Data")).click();
+    wd.findElement(By.name("width")).click();
+    wd.findElement(By.name("width")).clear();
+    wd.findElement(By.name("width")).sendKeys(new CharSequence[] { "10" });
+    wd.findElement(By.name("length")).click();
+    wd.findElement(By.name("length")).clear();
+    wd.findElement(By.name("length")).sendKeys(new CharSequence[] { "20" });
+    wd.findElement(By.cssSelector("input.btn")).click();
+    wd.findElement(By.cssSelector("input.btn")).click();
+    wd.findElement(By.name("width")).click();
+    wd.findElement(By.name("width")).clear();
+    wd.findElement(By.name("width")).sendKeys(new CharSequence[] { "12" });
+    wd.findElement(By.name("length")).click();
+    wd.findElement(By.name("length")).clear();
+    wd.findElement(By.name("length")).sendKeys(new CharSequence[] { "33" });
+    wd.findElement(By.cssSelector("input.btn")).click();
+  }
+  
+  @After
+  public void tearDown()
+  {
+    wd.quit();
+  }
+}
